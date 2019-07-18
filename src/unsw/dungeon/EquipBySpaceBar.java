9059ebe7f20eea1c1f6918a56bf681107f5f6ae6
Stephen <fongchinghinunsw@gmail.com>
2019-07-19 08:11:14 +1000

    dungeon: Add EquipBySpaceBar and EquipByStandOn class

---
+++ src/unsw/dungeon/EquipBySpaceBar.java
@@ -0,0 +1,11 @@
+package unsw.dungeon;
+
+public class EquipBySpaceBar implements EquipWay {
+
+	@Override
+	public void equip() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/unsw/dungeon/EquipByStandOn.java
@@ -0,0 +1,11 @@
+package unsw.dungeon;
+
+public class EquipByStandOn implements EquipWay {
+
+	@Override
+	public void equip() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
