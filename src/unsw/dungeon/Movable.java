502bcf7a2123fc6740833203bcc3b626c6c3bd27
jou-98 <yingchennie@gmail.com>
2019-07-20 17:30:20 +1000

    Movable: now player are stopped by closed doors in all directions

--- src/unsw/dungeon/Movable.java
+++ src/unsw/dungeon/Movable.java
@@ -16,25 +16,35 @@ public abstract class Movable extends Entity {
 	}
 
 	public void moveUp() {
-		if (getY() > 0 && !(dungeon.sameClass(getX(), getY() - 1, "Wall")))
-			y().set(getY() - 1);
+		if (getY() > 0 && !(dungeon.sameClass(getX(), getY() - 1, "Wall"))) {
+			if (dungeon.canStepOn(getX(), getY() - 1)) {
+				y().set(getY() - 1);
+			}
+		}
 	}
 
 	public void moveDown() {
-		if (getY() < dungeon.getHeight() - 1 && !(dungeon.sameClass(getX(), getY() + 1, "Wall")))
+		if (getY() < dungeon.getHeight() - 1 && !(dungeon.sameClass(getX(), getY() + 1, "Wall"))) {
 			if (dungeon.canStepOn(getX(), getY() + 1)) {
 				y().set(getY() + 1);
 			}
+		}
 	}
 
 	public void moveLeft() {
-		if (getX() > 0 && !(dungeon.sameClass(getX() - 1, getY(), "Wall")))
-			x().set(getX() - 1);
+		if (getX() > 0 && !(dungeon.sameClass(getX() - 1, getY(), "Wall"))) {
+			if (dungeon.canStepOn(getX() - 1, getY())) {
+				x().set(getX() - 1);
+			}
+		}
 	}
 
 	public void moveRight() {
-		if (getX() < dungeon.getWidth() - 1 && !(dungeon.sameClass(getX() + 1, getY(), "Wall")))
-			x().set(getX() + 1);
+		if (getX() < dungeon.getWidth() - 1 && !(dungeon.sameClass(getX() + 1, getY(), "Wall"))) {
+			if (dungeon.canStepOn(getX() + 1, getY())) {
+				x().set(getX() + 1);
+			}
+		}
 	}
 
 	public abstract long getSpeed();
