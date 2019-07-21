88b45a83b40c8e06f0ca916d796b68af8c39d089
jou-98 <yingchennie@gmail.com>
2019-07-21 15:02:53 +1000

    US10_Test: added testPickup and testOneAtATime

--- src/unsw/dungeon/US10_Test.java
+++ src/unsw/dungeon/US10_Test.java
@@ -24,6 +24,37 @@ public class US10_Test {
 		assertEquals(sword.getDurability(), 0, "Durability cannot be decreased");
 		sword.reduceDurability();
 		assertEquals(sword.getDurability(), 0, "Durability still changes after turnin 0");
-
 	}
+
+	@Test
+	void testPickUpSword() {
+		final JFXPanel fxPanel = new JFXPanel();
+		Dungeon dungeon = new Dungeon(10, 10);
+		Player player = new Player(dungeon, 3, 4);
+		dungeon.setPlayer(player);
+		dungeon.addEntity(player);
+		Sword sword = new Sword(3, 4);
+		dungeon.addEntity(sword);
+		assertTrue(player.equipItem(), "Cannot pick up sword");
+		assertNotNull(player.getBag().removeItem("Sword"), "Sword not in bag");
+	}
+
+	@Test
+	void testOneAtATime() {
+		final JFXPanel fxPanel = new JFXPanel();
+		Dungeon dungeon = new Dungeon(10, 10);
+		Player player = new Player(dungeon, 3, 4);
+		dungeon.setPlayer(player);
+		dungeon.addEntity(player);
+		Sword sword = new Sword(3, 4);
+		dungeon.addEntity(sword);
+		player.equipItem();
+		Sword sword2 = new Sword(2, 4);
+		dungeon.addEntity(sword);
+		assertEquals(player.countSwordInBackPack(), 1, "Count of sword wrong");
+		player.setX(2);
+		assertFalse(player.equipItem(), "Picking up a second sword");
+		assertEquals(player.countSwordInBackPack(), 1, "Count of sword wrong");
+	}
+
 }
