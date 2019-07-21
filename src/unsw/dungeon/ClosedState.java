bdc9383a37296e9739c6d455bc483836a2c832c3
Stephen <fongchinghinunsw@gmail.com>
2019-07-21 16:16:48 +1000

    DoorState: remove unused abstract method StandOn

--- src/unsw/dungeon/ClosedState.java
+++ src/unsw/dungeon/ClosedState.java
@@ -2,11 +2,6 @@ package unsw.dungeon;
 
 public class ClosedState implements DoorState {
 
-	@Override
-	public void standOn() {
-
-	}
-
 	public boolean shallPass() {
 		return false;
 	}
--- src/unsw/dungeon/OpenState.java
+++ src/unsw/dungeon/OpenState.java
@@ -2,11 +2,6 @@ package unsw.dungeon;
 
 public class OpenState implements DoorState {
 
-	@Override
-	public void standOn() {
-
-	}
-
 	public boolean shallPass() {
 		return true;
 	}
