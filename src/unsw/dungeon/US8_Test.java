a8a9d2e13567472645d047a04ac7e84fd3909b19
jou-98 <yingchennie@gmail.com>
2019-07-21 12:12:41 +1000

    US8_Test: tests added

--- src/unsw/dungeon/US8_Test.java
+++ src/unsw/dungeon/US8_Test.java
@@ -5,6 +5,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+
+import javafx.embed.swing.JFXPanel;
 
 public class US8_Test {
 
@@ -28,8 +31,76 @@ public class US8_Test {
 		bomb.light();
 		assertTrue(bomb.lit(), "Bomb not lit");
 		player.getBag().removeItem("Bomb");
+		assertNull(player.getBomb(),"Bomb not removed");
 		bomb.explode();
 		assertTrue(bomb.exploded(), "Bomb didn't explode");
+	}
+
+	@Test
+	public void testBombBlowUp() {
+		final JFXPanel fxPanel = new JFXPanel();
+		Dungeon dungeon = new Dungeon(10, 10);
+		Player player = new Player(dungeon, 3, 3);
+		Bomb bomb = new Bomb(dungeon, 3, 3);
+		dungeon.setPlayer(player);
+		dungeon.addBomb(bomb);
+		dungeon.addEntity(bomb);
+		player.equipItem();
+		player.setX(7);
+		player.setY(4);
+		Enemy enemy = new Enemy(dungeon, 7, 3);
+		dungeon.addEnemy(enemy);
+		dungeon.addEntity(player);
+		dungeon.addEntity(enemy);
+		Bomb bombFromBag = player.getBomb();
+		assertNotNull(bombFromBag, "Picked up bomb is null");
+		bomb.setX(player.getX());
+		bomb.setY(player.getY());
+		assertEquals(bomb.getX(), 7, "bomb not placed on right x value");
+		assertEquals(bomb.getY(), 4, "bomb not placed on right y value");
+		assertTrue(bomb.samePlace(player.getX(), player.getY()), "Player not in same place as bomb");
+		assertTrue(bomb.adjacent(enemy.getX(), enemy.getY())||bomb.samePlace(enemy.getX(), enemy.getY()), "Enemy not adjacent to bomb");
+		dungeon.addObservers();
+		dungeon.addEntity(bomb);
+		bomb.light();
+		dungeon.removeEquippedEntity(7, 4, "Bomb");
+		assertTrue(bomb.lit(), "Bomb not lit");
+		assertTrue(dungeon.hasBomb(7, 4), "Bomb not in dungeon");
+		player.getBag().removeItem("Bomb");
+		bomb.notifyObservers();
+		bomb.explode();
+		assertFalse(enemy.isAlive().getValue(), "Enemy not killed by bomb");
+		assertFalse(player.isAlive(), "Player not killed by bomb");
+		assertTrue(bomb.exploded(), "Bomb didn't explode");
+	}
+
+	@Test
+	public void testOneBomb() {
+		final JFXPanel fxPanel = new JFXPanel();
+		Dungeon dungeon = new Dungeon(10, 10);
+		Player player = new Player(dungeon, 3, 3);
+		Bomb bomb = new Bomb(dungeon, 3, 3);
+		dungeon.setPlayer(player);
+		dungeon.addBomb(bomb);
+		dungeon.addEntity(bomb);
+		Bomb bomb1 = new Bomb(dungeon, 7, 4);
+		dungeon.addBomb(bomb1);
+		dungeon.addEntity(bomb1);
+		dungeon.addEntity(player);
+		player.equipItem();
+		player.setX(7);
+		player.setY(4);
+		player.equipItem();
+		player.setX(8);
+		bomb.setX(player.getX());
+		bomb.setY(player.getY());
+		dungeon.addObservers();
+		dungeon.addEntity(bomb);
+		bomb.light();
+		dungeon.removeEquippedEntity(7, 4, "Bomb");
+		player.getBag().removeItem("Bomb");
+		bomb.notifyObservers();
+		assertTrue(dungeon.hasBomb(7, 4), "Allowing second bomb in same place");
 	}
 
 }
