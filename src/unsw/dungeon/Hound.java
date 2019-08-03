1f52de2eaa81bcbf7319839227d4615700e71d8d
jou-98 <yingchennie@gmail.com>
2019-08-04 01:10:41 +1000

    Nothing

--- src/unsw/dungeon/Hound.java
+++ src/unsw/dungeon/Hound.java
@@ -4,7 +4,7 @@ public class Hound extends Enemy {
 
 	public Hound(Dungeon dungeon, int x, int y) {
 		super(dungeon, x, y);
-		this.setSpeedFactor(new Normal());
+		this.setSpeedFactor(new Slow());
 	}
 
 }
