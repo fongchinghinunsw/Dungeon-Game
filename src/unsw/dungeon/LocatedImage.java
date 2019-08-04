ff7490f0717112c692c85f4d5b09528d37ff4590
jou-98 <yingchennie@gmail.com>
2019-08-04 17:04:47 +1000

    Mage:Javadoc up to here

--- src/unsw/dungeon/LocatedImage.java
+++ src/unsw/dungeon/LocatedImage.java
@@ -2,15 +2,32 @@ package unsw.dungeon;
 
 import javafx.scene.image.Image;
 
+/**
+ * LocatedImage is just an Image object that stores the url of the image By
+ * having this we can remove the right image though it is bad design :)
+ * 
+ * @author z5211173
+ *
+ */
 public class LocatedImage extends Image {
 
 	private String url;
-	
+
+	/**
+	 * constructor for located image
+	 * 
+	 * @param url
+	 */
 	public LocatedImage(String url) {
 		super(url);
 		this.url = url;
 	}
-	
+
+	/**
+	 * getter for url
+	 * 
+	 * @return url in a string
+	 */
 	public String getUrl() {
 		return url;
 	}
